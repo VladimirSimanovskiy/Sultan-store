cd7041ce2bead512fd49b9d4cac92993909c5fa7
Vladimir Simanovskii <v.simanovsky@gmail.com>
2023-04-03 00:26:34 +0700

    routes

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename='/Sultan'>
+    <BrowserRouter basename='/Sultan-store'>
       <App />
     </BrowserRouter>
   </Provider>
